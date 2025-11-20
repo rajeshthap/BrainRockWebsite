e00b363d6d80a2c7aca55fbeb01b1d3fd4e3dd44
rajeshthap <jdmjvm@gmail.comgit config user.email>
2025-11-20 17:53:12 +0530

    Footer Ui done

--- src/componets/footer/FooterPage.js
+++ src/componets/footer/FooterPage.js
@@ -1,11 +1,15 @@
 import React from 'react'
-import { Col, Row } from 'react-bootstrap'
+import { Col, Form, Row } from 'react-bootstrap'
 import { FaInstagram, FaLinkedinIn, FaTwitter } from 'react-icons/fa'
 import Logogoogleimg from "../../assets/images/google-img.png";
 import BrLogo from "../../assets/images/brainrock_logo.png"
 import { TiSocialFacebook, TiSocialFacebookCircular } from "react-icons/ti";
 import "../../assets/css/Footer.css";
-
+import { MdKeyboardArrowRight } from "react-icons/md";
+import { MdArrowForwardIos } from "react-icons/md";
+import Logo from "../../assets/images/brainrock_logo.png";
+import fImge from "../../assets/images/de-1.jpg";
+import FImg1 from "../../assets/images/de-2.jpg";
 function FooterPage() {
   return (
     <div>
@@ -15,72 +19,128 @@ function FooterPage() {
 
           <div className='footer-widget about-us'>
 
-            <div className='footer-logo mb-30'></div>
-            <p>saSASASASA</p>
+            <div className='footer-logo mb-30'><img src={Logo} alt="logo" className="logo-wecd" /></div>
+
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod temp incididunt ut labore et dolore</p>
+
             <div className='subscribe-area'>
-
-
+              <Form className=''>
+                <span className='btn-shape'></span>
+                <Form.Control type="email" placeholder="Enter email" className='input-style-4' />
+                <button className='btn-subs'> <i><MdKeyboardArrowRight /></i></button>
+              </Form>
 
             </div>
 
           </div>
-
-
-        </Col>
-        <Col lg={2} md={6} sm={6}>1</Col>
-        <Col lg={4} md={6} sm={6}>1</Col>
-        <Col lg={2} md={6} sm={6}>1</Col>
-      </Row>
-
-
-      <Row className=' p-4'>
-        <Col lg={3} md={3} sm={12} className='br-footer-details'>
-          <div>
-            <img src={BrLogo} alt="br-logo" className='img-fluid google-img'></img>
-          </div>
-          <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
-
-          <div>
-            <img src={Logogoogleimg} alt="logo" className='img-fluid google-img'></img>
-          </div>
-        </Col>
-        <Col lg={3} md={3} sm={12} className='footer-li'>
-          <div className="br-footer-heading">
-            <h4 className=''>Link</h4>
-          </div>
-          <ul type="none">
-            <li>About Us</li>
-            <li>services</li>
-            <li>Case Studies</li>
-            <li>How it Work</li>
-            <li>Blog</li>
-            <li>Careers</li>
-            <li>Areas We Serve</li>
-          </ul>
-
-
-        </Col>
-        <Col lg={3} md={3} sm={12} className="br-footer-details">
-          <div className="br-footer-heading">
-            <h4>Our Head Office</h4>
-          </div>
-          <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
-
-          <p>+91-8193991148<br></br> admin@brainrock.in</p>
-
-        </Col>
-        <Col lg={3} md={3} sm={12}>
-          <div>
+          <div className='mt-3'>
 
             <TiSocialFacebook className='footer-icon' />
             <FaInstagram className='footer-icon' />
             <FaTwitter className='footer-icon' />
             <FaLinkedinIn className='footer-icon' />
           </div>
+
         </Col>
+        <Col lg={2} md={6} sm={6}>
+
+          <div className='footer-widget footer-link'>
+
+            <h4 className='footer-widget-title'>
+              Important Links
+
+              <span className='footer-title-line'></span>
+            </h4>
+            <ul className='footer-list'>
+
+              <li> <i><MdArrowForwardIos className='i-space' /></i>About Us</li>
+              <li> <i><MdArrowForwardIos className='i-space' /></i>Meet Our Team</li>
+              <li> <i><MdArrowForwardIos className='i-space' /></i>Our Projects</li>
+              <li> <i><MdArrowForwardIos className='i-space' /></i>News & Media</li>
+              <li> <i><MdArrowForwardIos className='i-space' /></i>Contact us</li>
 
 
+
+
+
+            </ul>
+
+          </div>
+
+
+        </Col>
+        <Col lg={4} md={6} sm={6}>
+          <div className='footer-widget footer-recent-post'>
+            <h4 className='footer-widget-title'>
+              Working Hours
+
+              <span className='footer-title-line'></span>
+            </h4>
+            <div className='recent-post-wrp'>
+              <div className='footer-recent-post-single'>
+
+                <div className='recent-post-pic'> <img src={fImge} alt="fImge" className="" /></div>
+
+                <div className='recent-post-desc'>
+                  <h5>
+                    Fruitcake cotton candy<br></br>
+                    wafer pudding
+                  </h5>
+                  <span className='recent-post-date'>17 April 2023</span>
+                </div>
+              </div>
+            </div>
+            <div className='recent-post-wrp'>
+              <div className='footer-recent-post-single'>
+
+                <div className='recent-post-pic'> <img src={FImg1} alt="FImg1" className="" /></div>
+
+                <div className='recent-post-desc'>
+                  <h5>
+                    Muffin apple pie tootsie
+                    <br></br>
+                    sweet cotton roll
+                  </h5>
+                  <span className='recent-post-date'>18 April 2023</span>
+                </div>
+              </div>
+            </div>
+          </div>
+
+
+
+        </Col>
+        <Col lg={2} md={6} sm={6}>
+
+          <div className='footer-widget footer-link'>
+
+            <h4 className='footer-widget-title'>
+              IT Services
+
+              <span className='footer-title-line'></span>
+            </h4>
+            <ul className='footer-list'>
+
+              <li> <i><MdArrowForwardIos className='i-space' /></i>Web Design</li>
+              <li> <i><MdArrowForwardIos className='i-space' /></i>It Services</li>
+              <li> <i><MdArrowForwardIos className='i-space' /></i>Industries Services</li>
+              <li> <i><MdArrowForwardIos className='i-space' /></i>Feature Box</li>
+              <li> <i><MdArrowForwardIos className='i-space' /></i>Tab Service</li>
+
+
+
+
+
+            </ul>
+
+          </div>
+
+
+        </Col>
       </Row>
+
+
+
 
     </div>
   )
