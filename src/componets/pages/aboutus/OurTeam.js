785766058d0796e1f66f5049a7a6951f309e8b71
rajeshthap <jdmjvm@gmail.comgit config user.email>
2025-12-01 16:29:09 +0530

    images chnages

--- src/componets/pages/aboutus/OurTeam.js
+++ src/componets/pages/aboutus/OurTeam.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Container } from 'react-bootstrap';
+import { Container, Row } from 'react-bootstrap';
 import "../../../assets/css/aboutus.css";
 
 function OurTeam() {
@@ -7,7 +7,48 @@ function OurTeam() {
     <div className="ourteam-section">
       <Container>
         <div className="ourteam-box">
-          <h2>Our Team</h2>
+           <Row className='mt-5'>
+          <div >
+            <h1> OUR Team</h1>
+             <section className="our-team-section">
+      <div className="container">
+        <Row>
+          <div className="col-lg-3 col-md-6 col-sm-6">
+            <div className="our-team ">
+              <div className="pic">
+                <img 
+                  src="https://i.ibb.co/8x9xK4H/team.jpg" 
+                  alt="team"
+                />
+              </div>
+
+              <div className="team-content">
+                <h3 className="title">Team 1</h3>
+                <span className="post">Inhaber & Geschäftsführer</span>
+              </div>
+
+              <ul className="social">
+                <li>
+                  <a href="#" target="_blank" rel="noopener noreferrer" className="fa fa-facebook"></a>
+                </li>
+                <li>
+                  <a href="#" className="fa fa-twitter"></a>
+                </li>
+                <li>
+                  <a href="#" className="fa fa-google-plus"></a>
+                </li>
+                <li>
+                  <a href="#" className="fa fa-linkedin"></a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </Row>
+      </div>
+    </section>
+
+          </div>
+        </Row>
         
         </div>
       </Container>
